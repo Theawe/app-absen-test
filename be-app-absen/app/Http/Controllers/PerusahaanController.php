c79b8fa3f34870493d42e5a3756f52827535c1dd
Yordy Wirasatwika A <wiraadrai@gmail.com>
2023-08-31 16:14:29 +0700

    Add Some Changes

--- be-app-absen/app/Http/Controllers/PerusahaanController.php
+++ be-app-absen/app/Http/Controllers/PerusahaanController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostPerusahaanRequest;
-use App\Models\Karyawan;
 use App\Models\Perusahaan;
-use Illuminate\Http\Request;
 
 class PerusahaanController extends BaseController
 {
@@ -23,42 +21,6 @@ class PerusahaanController extends BaseController
         ]);
         return response()->json(
             $peruahaan,
-            200
-        );
-    }
-
-    public function insertToMySql(Request $request)
-    {
-        $peruahaan =  Perusahaan::create([
-            'nama_perusahaan' => $request->nama_perusahaan,
-            'alamat_perusahaan' => $request->alamat_perusahaan
-        ]);
-        return response()->json(
-            $peruahaan,
-            200
-        );
-    }
-
-    public function insertToMongo(Request $request)
-    {
-        $this->validate($request, [
-            'nama_karyawan' => 'required|string',
-            'umur_karyawan' => 'required|integer',
-            'alamat_karyawan' => 'required|string',
-            'nama_perusahaan' => 'required|string'
-        ]);
-
-        $karyawan =  Karyawan::create(
-            [
-                'nama_karyawan' => $request->nama_karyawan,
-                'umur_karyawan' => $request->umur_karyawan,
-                'alamat_karyawan' => $request->alamat_karyawan,
-                'nama_perusahaan' => $request->nama_perusahaan
-            ]
-        );
-
-        return response()->json(
-            $karyawan,
             200
         );
     }
